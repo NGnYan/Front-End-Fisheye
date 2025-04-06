4d2d5a3e513b8ba1c62df62f34a9a67c6e3fcf73
NGnYan <ngnyan@air-de-ngnyan.home>
2025-04-06 12:58:00 +0200

    importation des datas

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,33 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, portrait, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const localisation = document.createElement("p");
+    localisation.textContent = `${city}, ${country}`;
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(localisation);
+
+    return article;
+  }
+
+  return {
+    name,
+    picture,
+    id,
+    city,
+    country,
+    portrait,
+    tagline,
+    price,
+    getUserCardDOM,
+  };
 }
