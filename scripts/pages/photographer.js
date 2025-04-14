dd28fc76c48f89fb043659cf8e302dee2a9736d0
NGnYan <ngnyan@air-de-ngnyan.home>
2025-04-14 21:11:52 +0200

    update datas photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -27,11 +27,35 @@ async function init() {
   const photographers = await getPhotographers();
   const photographer = photographers.find((p) => p.id == photographerId);
 
-  if (photographer) {
-    displayPhotographer(photographer);
-  } else {
-    console.error("Photographe non trouvé");
+  // Appel API
+  async function getPhotographers() {
+    const response = await fetch("./data/photographers.json");
+    const data = await response.json();
+    return data.photographers;
   }
+
+  // Fonction pour afficher les informations du photographe en utilisant le template
+  function displayPhotographer(photographer) {
+    const photographersHeader = document.querySelector(".photograph-header");
+
+    // Utilisation du template pour obtenir le DOM du photographe
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+
+    // Ajout du DOM du photographe à la section header
+    photographersHeader.appendChild(userCardDOM);
+  }
+
+  // Fonction principale pour initialiser la page
+  async function init() {
+    const photographerId = getPhotographerId();
+    const photographers = await getPhotographers();
+    const photographer = photographers.find((p) => p.id == photographerId);
+
+    displayPhotographer(photographer);
+  }
+
+  init();
 }
 
 init();
